cbad644040a68ac77c66cc7be9f22651ef8c031f
tisha-akter <tishaakter.jui79@gmail.com>
2023-04-12 13:22:00 +0600

    showed appliedjobs data

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AppliedJobs = () => {
+    const [jobType, setJobType] = useState("");
+    const [jobsData, setJobsData] = useState([]);
+    const appliedJobsLocal = JSON.parse(localStorage.getItem('appliedJobs')) || [];
+
+    // appliedJobs.map(job => console.log(job.title));
+
+    useEffect(() => {
+        fetch('FeaturedJobs.json')
+            .then(response => response.json())
+            .then(data => setJobsData(data))
+            .catch(error => console.error(error));
+    }, []);
+
+    // applied jobs 
+    const appliedJobsList = [];
+
+    // looping through localStorage data and finding matched data 
+    for (const appliedJobLocal of appliedJobsLocal) {
+        const appliedJob = jobsData.find(job => appliedJobLocal.id == job.id);
+        appliedJobsList.push(appliedJob);
+    }
+
+    const onsiteJobsList = appliedJobsList.filter(job => job?.job_type == "Onsite");
+    const RemoteJobsList = appliedJobsList.filter(job => job?.job_type == "Remote");
+
+    console.log(appliedJobsList);
+    console.log("onsite", onsiteJobsList);
+    console.log("remote", RemoteJobsList);
+
+
     return (
         <div>
             {/* banner  */}
             <div className='bg-indigo-50 text-center'>
                 <h3 className='pb-28 py-20 text-2xl font-bold '>Applied Jobs</h3>
             </div>
-            
+
             <div className='container px-5 py-24 mx-auto items-center'>
 
                 {/* onsite or remote  */}
                 <div className='flex justify-center items-center'>
                     <div className='mb-5'>
-                        <button type="button" className="border border-indigo-500 hover:bg-indigo-500 rounded-lg py-2 px-4 font-semibold ml-2 focus:outline-none focus:shadow-outline mb-4 sm:mb-0">Remote</button>
-                        <button type="button" className="border border-indigo-500 hover:text-white text-indigo-500 hover:bg-indigo-700 rounded-lg py-2 px-4 font-semibold ml-2 focus:outline-none focus:shadow-outline mb-4 sm:mb-0">Onsite</button>
+                        <button onClick={() => setJobType("Remote")} type="button" className="border border-indigo-500 hover:bg-indigo-500 rounded-lg py-2 px-4 font-semibold ml-2 focus:outline-none focus:shadow-outline mb-4 sm:mb-0">Remote</button>
+                        <button onClick={() => setJobType("Onsite")} type="button" className="border border-indigo-500 hover:text-white text-indigo-500 hover:bg-indigo-700 rounded-lg py-2 px-4 font-semibold ml-2 focus:outline-none focus:shadow-outline mb-4 sm:mb-0">Onsite</button>
                     </div>
                 </div>
 
@@ -22,20 +52,22 @@ const AppliedJobs = () => {
                 <section className="text-gray-600 body-font">
                     <div className="items-center">
                         <div className="border rounded-md w-4/5 p-4 mx-auto ">
-
-                            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
+                            {appliedJobsList.map(job => (
+                            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left" key={job?.id}>
+                                <div className='w-48 h-48 border items-center'>
+                                    <img alt="team" className="flex-shrink-0 rounded-lg items-center pt-16 scroll-px-4 pl-5 sm:mb-0 mb-4" src={job?.company_logo}/>
+                                </div>
                                 <div className="flex-grow sm:pl-8">
-                                    <h2 className="title-font font-medium text-lg text-gray-900 mb-3">Holden Caulfield</h2>
-                                    <h3 className="text-gray-500 mb-4">UI Developer</h3>
+                                    <h2 className="title-font font-medium text-lg text-gray-900 mb-3"> {job?.tittle} </h2>
+                                    <h3 className="text-gray-500 mb-4">{job?.company_name}</h3>
 
                                     <div className='flex justify-between'>
                                         <div className='flex flex-wrap gap-4'>
                                             <button className='border border-indigo-500 hover:bg-indigo-500 hover:text-white text-indigo-500 px-2 py-0 rounded mr-2'>
-                                                <p>remote</p>
+                                                <p>{job?.job_type}</p>
                                             </button>
                                             <button className='border border-indigo-500 hover:bg-indigo-500 hover:text-white text-indigo-500 px-2 py-0 rounded mr-2'>
-                                                <p>fulltime</p>
+                                                <p>{job?.employment_type}</p>
                                             </button>
                                         </div>
                                         <div className=''>
@@ -45,19 +77,20 @@ const AppliedJobs = () => {
 
                                     <div className='py-2 flex gap-8 mb-4 mt-3'>
                                         <p className='text-sm text-gray-600'>
-                                            <img src="" className='inline-block w-4 h-4 mr-2' alt='Address Icon' />
-                                            Location
+                                            <img src={job?.location?.icon} className='inline-block w-4 h-4 mr-2' alt='Address Icon' />
+                                            {job?.location?.address}
                                         </p>
                                         <p className='text-sm text-gray-600'>
-                                            <img src="" className='inline-block w-4 h-4 mr-2' alt='Salary Icon' />
-                                            Salary:
+                                            <img src={job?.salary.icon} className='inline-block w-4 h-4 mr-2' alt='Salary Icon' />
+                                            {job?.salary.range}
                                         </p>
                                     </div>
 
                                 </div>
                             </div>
-
+                            ))}
                         </div>
+                        
                     </div>
                 </section>
             </div>
@@ -67,3 +100,6 @@ const AppliedJobs = () => {
 };
 
 export default AppliedJobs;
+
+
+
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -6,6 +6,25 @@ const JobDetails = () => {
     const allJobs = useLoaderData();
     const jobInfo = allJobs.filter(job => job.id == jobId);
 
+    
+    const isApplied = localStorage.getItem('appliedJobs') && JSON.parse(localStorage.getItem('appliedJobs')).some(job => job.id == jobInfo[0].id);
+
+    const handleApplyNow = () => {
+        const appliedJobs = JSON.parse(localStorage.getItem('appliedJobs')) || [];
+        const job = {
+            id: jobInfo[0].id,
+            title: jobInfo[0].job_title.tittle,
+            company: jobInfo[0].company_name,
+            location: jobInfo[0].location.address,
+            salary: jobInfo[0].salary.range,
+            dateApplied: new Date().toLocaleDateString(),
+        };
+        appliedJobs.push(job);
+        localStorage.setItem('appliedJobs', JSON.stringify(appliedJobs));
+        window.location.href = '/applied-jobs'; // navigate to the applied jobs page
+    };
+
+
     return (
         <div className=''>
             <div className='bg-indigo-50 text-center'>
@@ -18,35 +37,35 @@ const JobDetails = () => {
                         <p className='mb-4'><span className='font-bold'>Job Responsibility:</span> {jobInfo[0].job_responsibilities}</p>
                         <p className='mb-4'><span className='font-bold'>Educational Requirements: </span>{jobInfo[0].educational_requirements}</p>
                         <p className='mb-4'><span className='font-bold'>Experiences: </span>{jobInfo[0].experience_requirements}</p>
-
                     </div>
                     <div className="lg:w-2/6 md:w-1/2 bg-indigo-50 rounded-md p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
                         <h4 className='font-bold text-xl mb-3'>Job Details</h4>
                         <hr className='mb-3' />
-
                         <p className='mb-3'><img src={jobInfo[0].salary.icon} alt="" className='inline-block w-4 h-4 mr-2' /> <span>Salary :</span>{jobInfo[0].salary.range}</p>
                         <p className='mb-5'><img src={jobInfo[0].job_title.icon} alt="" className='inline-block w-4 h-4 mr-2' /> <span>Job Title :</span>{jobInfo[0].job_title.tittle}</p>
-
                         <h4 className='font-bold text-xl mb-3'>Contact Information</h4>
                         <hr className='mb-3' />
-
                         <p className='mb-3'><img src={jobInfo[0].contact_information.phone.icon} alt="" className='inline-block w-4 h-4 mr-2' /> <span>Phone :</span>{jobInfo[0].contact_information.phone.number}</p>
                         <p className='mb-3'><img src={jobInfo[0].contact_information.email.icon} alt="" className='inline-block w-4 h-4 mr-2' /> <span>Email :</span>{jobInfo[0].contact_information.email.address}</p>
                         <p className='mb-3'><img src={jobInfo[0].location.icon} alt="" className='inline-block w-4 h-4 mr-2' /> <span>Address :</span>{jobInfo[0].location.address}</p>
-
                     </div>
                     <div className="flex items-center justify-center md:items-end md:justify-end   w-full mt-5">
-                        <button className="text-white bg-indigo-500 border-0 py-2 px-10 md:px-40 focus:outline-none hover:bg-indigo-600 rounded text-lg md:w-auto">
+                        
+                        {!isApplied && (
+                            <button className="text-white bg-indigo-500 border-0 py-2 px-10 md:px-40 focus:outline-none hover:bg-indigo-600 rounded text-lg md:w-auto" onClick={handleApplyNow}>
                             Apply Now
                         </button>
+                        )}
+                        {isApplied && (
+                            <p>Applied Successfully!</p>
+                        )}
                     </div>
                 </div>
             </section>
-
-
-
         </div>
     );
 };
 
 export default JobDetails;
+
+
